d80834f5401e30287cbcf06c52b249d17d28ed45
Eujinty Rathna <129171611+eujinty-rathna@users.noreply.github.com>
2023-04-26 14:37:20 +0530

    uploading redtape product scraped data with script
    i scraped the redtape websites footwear product data using scrapy frameworkd

---
+++ redtape/redtape/items.py
@@ -0,0 +1,18 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+from scrapy.item import  Item,Field
+
+
+class RedtapeItem(Item):
+
+    name = Field()
+    mrp  = Field()
+    price = Field()
+    colour = Field()
+    sizes = Field()
+    brand = Field()
+    model = Field()
+
